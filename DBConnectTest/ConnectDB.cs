24775f7f28dd177065a308f9b6dae3c0e2c01030
sawasick <50393229+sawasick@users.noreply.github.com>
2022-07-18 00:25:53 +0900

    プロジェクト ファイルを追加します。

---
+++ DBConnectTest/ConnectDB.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DBConnectTest
+{
+	public static class ConnectDB
+	{
+        public static string GetConnectionString()
+        {
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = "SAWASICKWIN10",
+                IntegratedSecurity = true,
+                InitialCatalog = "Test"
+                //UserID = "(ユーザー名)",
+                //Password = "(パスワード)"
+            };
+
+            return builder.ToString();
+        }
+
+        public static void Connect()
+		{
+            var table = new DataTable();
+            var connectionString = GetConnectionString();
+
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    // データベースの接続開始
+                    connection.Open();
+
+                    // SQLの実行
+                    command.CommandText = @"SELECT Id, Number, Name FROM MainTable";
+                    //command.ExecuteNonQuery();
+                    var adapter = new SqlDataAdapter(command);
+                    adapter.Fill(table);
+                    foreach(DataRow target in table.Rows)
+					{
+                        Console.WriteLine("DO");
+                        Console.WriteLine(target["Id"]);
+                        Console.WriteLine(target["Number"]);
+                        Console.WriteLine(target["Name"]);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    throw;
+                }
+                finally
+                {
+                    // データベースの接続終了
+                    connection.Close();
+                }
+            }
+        }
+	}
+}
---
+++ DBConnectTest/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace DBConnectTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(40, 36);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(698, 376);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Number";
+			this.columnHeader1.Width = 239;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Name";
+			this.columnHeader2.Width = 332;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+	}
+}
+
